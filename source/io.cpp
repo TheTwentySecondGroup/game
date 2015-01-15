bd196cc458e27e36481f59b7b5ed5f9bfda6f0b0
na3alf6 <chihiro.na3alf6@gmail.com>
2015-01-16 02:04:13 +0900

    fix joystick bug when using virtualbox additions

--- source/io.cpp
+++ source/io.cpp
@@ -19,12 +19,13 @@ void GameIO::routine() {
 		exit(EXIT_FAILURE);
 	}
 
-	if (joystick == NULL) {
+	if (joystick == NULL || SDL_JoystickOpened(0) == 0) {
 		axis0 = 0;
 		axis1 = 0;
 	} else {
 		axis0 = SDL_JoystickGetAxis(joystick, 0);
 		axis1 = SDL_JoystickGetAxis(joystick, 1);
+		cout <<SDL_NumJoysticks()<<"  " <<joystick << "axis0" << axis0 << "axis1" << axis1;
 	}
 
 	if (SdlKey[SDLK_UP] == SDL_PRESSED || axis1 < -30) {
@@ -63,70 +64,61 @@ void GameIO::routine() {
 	if (SdlKey[SDLK_RETURN] == SDL_PRESSED || jbutton[3] == 1) {
 		if (key[KEY_A] < KEY_MAX_COUNT)
 			key[KEY_A]++;
-	}
-	else {
+	} else {
 		key[KEY_A] = 0;
 	}
 
-	if(SdlKey[SDLK_1] == SDL_PRESSED || jbutton[0] == 1){
-		if(key[KEY_B] < KEY_MAX_COUNT)
+	if (SdlKey[SDLK_1] == SDL_PRESSED || jbutton[0] == 1) {
+		if (key[KEY_B] < KEY_MAX_COUNT)
 			key[KEY_B]++;
-	}
-	else {
+	} else {
 		key[KEY_B] = 0;
 	}
 
-    if(SdlKey[SDLK_2] == SDL_PRESSED || jbutton[1] == 1){
-		if(key[KEY_C] < KEY_MAX_COUNT)
+	if (SdlKey[SDLK_2] == SDL_PRESSED || jbutton[1] == 1) {
+		if (key[KEY_C] < KEY_MAX_COUNT)
 			key[KEY_C]++;
-	}
-	else {
+	} else {
 		key[KEY_C] = 0;
 	}
 
-	if(SdlKey[SDLK_3] == SDL_PRESSED || jbutton[2] == 1){
-		if(key[KEY_D] < KEY_MAX_COUNT)
+	if (SdlKey[SDLK_3] == SDL_PRESSED || jbutton[2] == 1) {
+		if (key[KEY_D] < KEY_MAX_COUNT)
 			key[KEY_D]++;
-	}
-	else {
+	} else {
 		key[KEY_D] = 0;
 	}
 
-	if(SdlKey[SDLK_4] == SDL_PRESSED || jbutton[3] == 1){
-		if(key[KEY_E] < KEY_MAX_COUNT)
+	if (SdlKey[SDLK_4] == SDL_PRESSED || jbutton[3] == 1) {
+		if (key[KEY_E] < KEY_MAX_COUNT)
 			key[KEY_E]++;
-	}
-	else {
+	} else {
 		key[KEY_E] = 0;
 	}
 
-	if(SdlKey[SDLK_5] == SDL_PRESSED || jbutton[2] == 1){
-		if(key[KEY_F] < KEY_MAX_COUNT)
+	if (SdlKey[SDLK_5] == SDL_PRESSED || jbutton[2] == 1) {
+		if (key[KEY_F] < KEY_MAX_COUNT)
 			key[KEY_F]++;
-	}
-	else {
+	} else {
 		key[KEY_F] = 0;
 	}
 
-	if(SdlKey[SDLK_6] == SDL_PRESSED || jbutton[2] == 1){
-		if(key[KEY_G] < KEY_MAX_COUNT)
+	if (SdlKey[SDLK_6] == SDL_PRESSED || jbutton[2] == 1) {
+		if (key[KEY_G] < KEY_MAX_COUNT)
 			key[KEY_G]++;
-	}
-	else {
+	} else {
 		key[KEY_G] = 0;
 	}
-	if(SdlKey[SDLK_7] == SDL_PRESSED || jbutton[2] == 1){
-		if(key[KEY_H] < KEY_MAX_COUNT)
+	if (SdlKey[SDLK_7] == SDL_PRESSED || jbutton[2] == 1) {
+		if (key[KEY_H] < KEY_MAX_COUNT)
 			key[KEY_H]++;
-	}
-	else {
+	} else {
 		key[KEY_H] = 0;
 	}
-	if(SdlKey[SDLK_8] == SDL_PRESSED || jbutton[2] == 1){
-		if(key[KEY_I] < KEY_MAX_COUNT)
+	if (SdlKey[SDLK_8] == SDL_PRESSED || jbutton[2] == 1) {
+		if (key[KEY_I] < KEY_MAX_COUNT)
 			key[KEY_I]++;
-	}
-	else {
+	} else {
 		key[KEY_I] = 0;
 	}
 }
@@ -135,10 +127,12 @@ GameIO::GameIO() {
 	// TODO Auto-generated constructor stub
 	for (int i = 0; i < KEY_MAX; i++)
 		key[i] = 0;
+	joystick = NULL;
+	if (SDL_NumJoysticks() > 0 &&strcmp(SDL_JoystickName(0),"VirtualBox mouse integration")!=0) {
 
-	joystick = SDL_JoystickOpen(0);	// ジョイスティックを開いて構造体に割り当てる（有効にする）．ここではジョイスティックは1つとして引数に0を指定
-	SDL_JoystickEventState(SDL_ENABLE);	// ジョイスティック関連のイベントを取得可能にする
-
+		joystick = SDL_JoystickOpen(0);	// ジョイスティックを開いて構造体に割り当てる（有効にする）．ここではジョイスティックは1つとして引数に0を指定
+		SDL_JoystickEventState(SDL_ENABLE);	// ジョイスティック関連のイベントを取得可能にする
+	}
 }
 
 GameIO::~GameIO() {
